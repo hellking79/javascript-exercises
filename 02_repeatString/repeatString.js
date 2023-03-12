61f15eca1c90ab1d3ce7c4401938475cb8168d09
Akilou <akilou.issifou1999@gmail.com>
2023-03-12 06:22:59 -0400

    im commiting for the second time

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+  let r=``
+  if(num<0){
+    return `ERROR`
+  }
+  for(i=0;i<num;i++){
+        r+=string
+    }
+    return r
 };
 
 // Do not edit below this line
