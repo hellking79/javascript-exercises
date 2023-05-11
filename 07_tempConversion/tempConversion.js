f6d307bc29377595629eaa5b5cc8a2d479b19d35
hellking79 <akilou.issifou1999@gmail.com>
2023-05-11 05:18:42 -0400

    Dont forget to upcase Math

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f,c) {
+  let cel =(c*9/5)+32;
+  let roundec=Math.round(cel*10)/10
+ 
+  let fell =(f-32)* 5/9
+  let roundef=Math.round(fell*10)/10
+  return{celsuis:roundec,
+  farenheit:roundef}; 
+   
+
 };
 
 const convertToFahrenheit = function() {
