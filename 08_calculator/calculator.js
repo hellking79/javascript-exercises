fea9dbf89083f4948b5729de2469235c3a46b4a8
hellking79 <akilou.issifou1999@gmail.com>
2023-05-21 05:12:13 -0400

    i forgot that i could use += to add tpgather all the nummbers in the array in this case multiply (*=)you use result=1  so that you can start multiplying with the same number as the first index since anything times 0 is 0 you use a[i]to tell it which index to start with and which number

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,18 +1,34 @@
-const add = function() {
+const add = function(a,b) {
+  let cal= a+b;
+  return cal;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  let cal=a-b;
+  return cal;
 	
 };
 
-const sum = function() {
-	
+
+const sum = function(a) {
+ let cal= a.reduce(add,0)
+  return cal;
+	};
+
+ const multiply = function(a) {
+if (a.length===0){
+  return 0;
+}
+let result=1;
+
+for(i=0;i<a.length;i++){
+result*=a[i]
+}
+return result;
 };
 
-const multiply = function() {
-
-};
+  
 
 const power = function() {
 	
