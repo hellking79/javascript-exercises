fe06e150d13ef6d3ec8bc0836d494e4e3a5261f2
hellking79 <akilou.issifou1999@gmail.com>
2023-06-03 04:35:05 -0400

    palindromes is bascicly a group of words that goes togather even if you write them backwords dont forget the include methodes it allows you to tell the computer if this variable/function includes this word then do this

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (p) {
+let bad=`ZZZZ`
+   if(p.includes(bad)){
+    return false
+}
+ else if (p=true){
+    return  p;
+}
 };
 
 // Do not edit below this line
