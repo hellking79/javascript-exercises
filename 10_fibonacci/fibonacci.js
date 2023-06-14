e684a534163aba004401684f13e15af13ff125f4
hellking79 <61370309+hellking79@users.noreply.github.com>
2023-06-14 05:17:25 -0400

    you dont have to seperate them just change the currentnum to 1

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {hi 
-
-};
-
-// Do not edit below this line
-module.exports = fibonacci;
+const fibonacci = function(n) {
+    let num=parseInt(n)
+    if (isNaN(num)|| num<0){
+        return `OOPS`
+    }
+    let prevnum1=1
+    let prevnum2=1
+    let currentnum=1
+    for(i=2;i<num;i++){
+        currentnum=prevnum1+prevnum2;
+         prevnum1=prevnum2
+         prevnum2=currentnum
+    }
+    return currentnum;
+    };
+    
+    // Do not edit below this line
+    module.exports = fibonacci;
+    
